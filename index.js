bfb494e18256fdbcc9db7ce7db08c161b4f35c2f
gajevski <mikolajxgajewski@gmail.com>
2023-08-03 14:07:36 +0200

    Serve simple test endpoint

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use(cors());
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+     res.setHeader("Access-Control-Allow-Credentials", "true");
+     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
+   next();
+ });
 
 app.use(bodyParser.json());
 app.use(
@@ -11,7 +21,7 @@ app.use(
 );
 
 app.get("/", (request, response) => {
-  response.json({ info: "APP init" });
+  response.json({ info: `Backend running on port ${port}.` });
 });
 
 app.listen(port, () => {
