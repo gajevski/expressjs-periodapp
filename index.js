9c6366340c80a08e6bf2e18839dd11c0991189bd
gajevski <mikolajxgajewski@gmail.com>
2023-08-04 12:36:22 +0200

    Return simple list in get request

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const app = express();
 const port = 3001;
 
 app.use(cors());
-app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-     res.setHeader("Access-Control-Allow-Credentials", "true");
-     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
-   next();
- });
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
+  );
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(
@@ -20,8 +23,13 @@ app.use(
   })
 );
 
-app.get("/", (request, response) => {
-  response.json({ info: `Backend running on port ${port}.` });
+app.get("/all-periods", (request, response) => {
+  response.json({
+    periods: [
+      { id: 0, description: "Period one" },
+      { id: 1, description: "Period two" },
+    ],
+  });
 });
 
 app.listen(port, () => {
