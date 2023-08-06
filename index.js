e1077135e7547fa5291c047cf8d1a9542feea9d8
gajevski <mikolajxgajewski@gmail.com>
2023-08-06 20:52:31 +0200

    Add edit period functionality

--- index.js
+++ index.js
@@ -39,6 +39,18 @@ app.post("/add-period", (request, response) => {
   response.json({ periods });
 });
 
+app.put("/edit-period", (request, response) => {
+  const periodId = request.body.id;
+  const periodIndex = periods.findIndex((period) => period.id === periodId);
+
+  if (periodIndex === -1) {
+    return response.status(404).json({ message: "User not found" });
+  }
+
+  periods[periodIndex] = request.body;
+  response.json({ periods });
+});
+
 app.delete("/delete-period", (request, response) => {
   const periodId = request.body.id;
   const periodIndex = periods.findIndex((period) => period.id === periodId);
