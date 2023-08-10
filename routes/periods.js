23c76bce6fed576202d1e0deb28aa8300a4d66ad
gajevski <mikolajxgajewski@gmail.com>
2023-08-10 20:32:07 +0200

    Change dates

--- routes/periods.js
+++ routes/periods.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 let periods = [
-  { id: 0, startDate: "2023-08-08T12:34:56.789Z", description: "Period one" },
-  { id: 1, startDate: "2023-08-08T12:34:56.789Z", description: "Period two" },
-  { id: 2, startDate: "2023-08-08T12:34:56.789Z", description: "Period three" },
+  { id: 0, startDate: "2023-06-10", description: "Period one" },
+  { id: 1, startDate: "2023-07-15", description: "Period two" },
+  { id: 2, startDate: "2023-08-21", description: "Period three" },
 ];
 
 function generateRandomId() {
