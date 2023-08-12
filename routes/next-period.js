71181beb06276dbebe99038a78f58287d002d3b7
gajevski <mikolajxgajewski@gmail.com>
2023-08-12 20:28:33 +0200

    Install momentJS to handle dates

--- routes/next-period.js
+++ routes/next-period.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const moment = require("moment");
 
-let nextPeriod = [
-    { id: 0, startDate: "2023-06-10", description: "Period one" },
-];
+let lastPeriod = { id: 0, startDate: "2023-06-10", description: "Period one" };
 
 router.get("/", (request, response) => {
-    response.json({ nextPeriod });
+    const lastStartDate = moment(lastPeriod.startDate);
+    const nextStartDate = lastStartDate.clone().add(26, "days").format("YYYY-MM-DD");
+
+    const nextPeriod = {
+        id: 1,
+        startDate: nextStartDate,
+        description: "Test",
+    };
+
+    response.json({ nextPeriod, lastPeriod }, );
 });
 
 module.exports = router;
